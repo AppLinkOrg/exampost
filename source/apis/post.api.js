52ac6a8b5bbaf91f7a7056e84f8f190cd2f8401c
alucard263096 <alucard263096@126.com>
2018-08-10 11:05:30 +0800

    添加显示我的赞

--- source/apis/post.api.js
+++ source/apis/post.api.js
@@ -620,5 +620,36 @@ export class PostApi
       }
     })
   }
+  //获取机构的基础信息
+  allmylike(json, callback, showLoading = true) {
+
+    if (showLoading)
+      ApiConfig.ShowLoading();
+
+    var header = ApiConfig.GetHeader();
+    console.log(header);
+    wx.request({
+      url: ApiConfig.GetApiUrl() + 'post/allmylike',
+      data: json,
+      method: 'POST',
+      dataType: 'json',
+      header: header,
+      success: function (res) {
+        if (callback != null) {
+          callback(res.data);
+        }
+      },
+      fail: function (res) {
+        console.log(res);
+        callback(false);
+      },
+      complete: function (res) {
+        console.log(res);
+
+        if (showLoading)
+          ApiConfig.CloseLoading();
+      }
+    })
+  }
 }
 
--- source/pages/memberinfo/memberinfo.js
+++ source/pages/memberinfo/memberinfo.js
@@ -1,9 +1,19 @@
 // pages/content/content.js
-import { AppBase } from "../../appbase";
-import { ApiConfig } from "../../apis/apiconfig";
-import { ContentApi } from "../../apis/content.api";
-import { MemberApi } from "../../apis/member.api";
-import { PostApi } from "../../apis/post.api";
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  ContentApi
+} from "../../apis/content.api";
+import {
+  MemberApi
+} from "../../apis/member.api";
+import {
+  PostApi
+} from "../../apis/post.api";
 
 class Content extends AppBase {
   constructor() {
@@ -11,23 +21,28 @@ class Content extends AppBase {
   }
   onLoad(options) {
     this.Base.Page = this;
-    options.id=4;
+    //options.id = 4;
     super.onLoad(options);
     this.Base.setMyData({
-      currenttab: "1",list:[],allmycomment:[],id:options.id
+      currenttab: "0",
+      list: [],
+      allmycomment: [],
+      id: options.id
     });
   }
   onMyShow() {
     var that = this;
     var memberApi = new MemberApi();
-    memberApi.info({fmember_id:this.Base.options.id}, (memberinfo) => {
+    memberApi.info({
+      fmember_id: this.Base.options.id
+    }, (memberinfo) => {
       if (memberinfo != null) {
-        this.Base.setMyData({ memberinfo: memberinfo });
+        this.Base.setMyData({
+          memberinfo: memberinfo
+        });
       }
     });
     this.loaddata();
-
-
   }
 
 
@@ -36,50 +51,63 @@ class Content extends AppBase {
     this.Base.setMyData({
       currenttab: e.detail.current
     });
-    this.loaddata();
   }
   changeTab(e) {
     console.log(e);
     this.Base.setMyData({
       currenttab: e.currentTarget.id
     });
-    this.loaddata();
   }
   loaddata(e) {
     console.log("loaddata");
     var api = new PostApi();
     var that = this;
-    var seq = parseInt(this.Base.getMyData().currenttab);
-    switch (seq) {
-      case 0:
-        api.list({ orderby: " r_main.post_time desc ", status: "A", member_id: this.Base.options.id }, (nlist) => {
-          
-          for (var i = 0; i < nlist.length; i++) {
-            if (nlist[i].images == "") {
 
-              nlist[i].images = [];
-            } else {
+    api.list({
+      orderby: " r_main.post_time desc ",
+      status: "A",
+      member_id: this.Base.options.id
+    }, (nlist) => {
 
-              nlist[i].images = nlist[i].images.split(",");
-            }
+      for (var i = 0; i < nlist.length; i++) {
+        if (nlist[i].images == "") {
 
-            nlist[i].timeduration = time_ago(nlist[i].post_time_timespan);
+          nlist[i].images = [];
+        } else {
 
-            //list.push(nlist[i]);
-          }
-          that.Base.setMyData({ list: nlist });
-        });
-        break;
-      case 1:
-        api.allmycomment({ member_id: this.Base.options.id}, (allmycomment) => {
-          for (var i = 0; i < allmycomment.length; i++) {
-            allmycomment[i].comment_time_ago = time_ago(allmycomment[i].comment_time_timespan);
-          }
-          that.Base.setMyData({ allmycomment });
-        });
-        break;
-    }
+          nlist[i].images = nlist[i].images.split(",");
+        }
+
+        nlist[i].timeduration = time_ago(nlist[i].post_time_timespan);
+
+        //list.push(nlist[i]);
+      }
+      that.Base.setMyData({
+        list: nlist
+      });
+    });
+    api.allmycomment({
+      member_id: this.Base.options.id
+    }, (allmycomment) => {
+      for (var i = 0; i < allmycomment.length; i++) {
+        allmycomment[i].comment_time_ago = time_ago(allmycomment[i].comment_time_timespan);
+      }
+      that.Base.setMyData({
+        allmycomment
+      });
+      });
+    api.allmylike({
+      member_id: this.Base.options.id
+    }, (allmylike) => {
+      for (var i = 0; i < allmylike.length; i++) {
+        allmylike[i].like_time_ago = time_ago(allmylike[i].like_time_timespan);
+      }
+      that.Base.setMyData({
+        allmylike
+      });
+    });
   }
+
   allfollow() {
     wx.navigateTo({
       url: '/pages/myfollow/myfollow',
@@ -91,21 +119,25 @@ class Content extends AppBase {
     })
   }
 
-  
-  addfriend(){
+
+  addfriend() {
     console.log("aaa");
     var api = new MemberApi();
     var that = this;
 
-    api.addfriend({follow_member_id:this.Base.options.id},(ret)=>{
+    api.addfriend({
+      follow_member_id: this.Base.options.id
+    }, (ret) => {
       wx.showToast({
         title: '关注成功',
-        icon:"none"
+        icon: "none"
       });
-      var memberinfo=that.Base.getMyData().memberinfo;
-      memberinfo.followed=true;
-      memberinfo.fanscount = memberinfo.fanscount+1;
-      that.Base.setMyData({memberinfo:memberinfo});
+      var memberinfo = that.Base.getMyData().memberinfo;
+      memberinfo.followed = true;
+      memberinfo.fanscount = memberinfo.fanscount + 1;
+      that.Base.setMyData({
+        memberinfo: memberinfo
+      });
     });
   }
   removefriend() {
@@ -113,7 +145,9 @@ class Content extends AppBase {
     var api = new MemberApi();
     var that = this;
 
-    api.removefriend({ follow_member_id: this.Base.options.id }, (ret) => {
+    api.removefriend({
+      follow_member_id: this.Base.options.id
+    }, (ret) => {
       wx.showToast({
         title: '已取消关注',
         icon: "none"
@@ -121,15 +155,17 @@ class Content extends AppBase {
       var memberinfo = that.Base.getMyData().memberinfo;
       memberinfo.followed = false;
       memberinfo.fanscount = memberinfo.fanscount - 1;
-      that.Base.setMyData({ memberinfo: memberinfo });
+      that.Base.setMyData({
+        memberinfo: memberinfo
+      });
     });
   }
   gotochatroom() {
 
     wx.navigateTo({
-      url: '/pages/chatroom/chatroom?member_id='+this.Base.options.id,
+      url: '/pages/chatroom/chatroom?member_id=' + this.Base.options.id,
     })
-  } 
+  }
 
 
   deletemypost(e) {
@@ -141,7 +177,9 @@ class Content extends AppBase {
       success(res) {
         if (res.confirm) {
           var postapi = new PostApi();
-          postapi._delete({ "idlist": id }, (ret) => {
+          postapi._delete({
+            "idlist": id
+          }, (ret) => {
 
             var list = [];
             var olist = that.Base.getMyData().list;
@@ -150,7 +188,9 @@ class Content extends AppBase {
                 list.push(olist[i]);
               }
             }
-            that.Base.setMyData({ list });
+            that.Base.setMyData({
+              list
+            });
           })
         }
       }
@@ -197,8 +237,8 @@ body.changeTab = content.changeTab;
 body.loaddata = content.loaddata;
 body.allpost = content.allpost;
 body.allfollow = content.allfollow;
-body.addfriend = content.addfriend; 
-body.removefriend = content.removefriend; 
+body.addfriend = content.addfriend;
+body.removefriend = content.removefriend;
 body.gotochatroom = content.gotochatroom;
 body.deletemypost = content.deletemypost;
 Page(body)
